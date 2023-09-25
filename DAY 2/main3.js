7ba61d62a93bd57e36cbc5c24e749a271ad0cbb3
ijatinkhatter <codexjatin@gmail.com>
2023-09-25 14:42:42 +0530

    day2-3

---
+++ DAY 2/main3.js
@@ -0,0 +1,26 @@
+// ----------------- TYPE OF OPERATOR ----------------
+
+// >>>>>>>>>>>>>> DATA TYPES (primitive data types) <<<<<<<<<<<<<<
+
+// string
+// number
+// boolean
+// undefined
+//  null
+// bigint
+// symbol  
+
+
+// let age = 21;
+// let firstName = 'jatin';
+// console.log(typeof age);
+
+
+// convert NUMBER to STRING
+// let age = +'33';
+// console.log(typeof age); 
+// console.log(age);
+
+// convert STRING to NUMBER
+let age = 33 + '';
+console.log(typeof age);
