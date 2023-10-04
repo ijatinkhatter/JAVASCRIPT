1b32d0b7295863bc546120cba11046c7d15d7d14
ijatinkhatter <codexjatin@gmail.com>
2023-10-05 00:03:00 +0530

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+// VARIABLES IN JAVASCRIPT
+
+const accountId = 123456789
+let accountEmail = "codexjatin@gmail.com"
+var accountPassword = "jkay1234"
+accountCity = "delhi"
+let accountState;
+
+/*
+    Prefer not to use VAR
+    because of issue in blocck scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// DATA TYPES 
+// PRIMITIVE DATA TYPES 
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// undefined
+// null => standalone value
+//symbol => use for uniqueness
+
+
+let num = 12345
+let name = "jatin"
+let value = true
+let state
+let age = null
+
+
+console.log(typeof name)
+console.log(typeof 12345)
+console.log(typeof true)
+console.log(typeof state)
+console.log(typeof age)
