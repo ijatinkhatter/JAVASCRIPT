88214d1102850a3fa239e3a9ce3c9104de2a6b49
ijatinkhatter <codexjatin@gmail.com>
2023-10-05 01:57:13 +0530

    conversion

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,44 @@
+// conversions in js
+
+// ------- STRING TO NUMBER ----------
+
+// let score = "33"
+// console.log(typeof score);
+
+// let newScore = Number(score)
+
+// console.log(typeof newScore);
+// console.log(newScore);
+
+
+
+// -------- NOT A NUMBER ("33abc") -------
+
+// let score = "33abc"
+// console.log(typeof score);
+
+// let newScore = Number(score)
+
+// console.log(typeof newScore);
+// console.log(newScore);
+
+
+// --------- BOOLEAN --------------------
+
+// let isBoolen
+// console.log(typeof isBoolen);
+
+// let isBooleanen = Boolean(isBoolen)
+
+// console.log(typeof isBooleanen);
+// console.log(isBooleanen);
+
+
+// ---------- NUMBER TO STRING ----------
+
+let score = 33
+console.log(typeof score);
+
+let newScore = String(score)
+console.log(typeof newScore);
+console.log(newScore);
