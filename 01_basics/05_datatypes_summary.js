1c85775abd7c9f03c4db0c454581e0d23fb84f62
ijatinkhatter <codexjatin@gmail.com>
2023-10-05 15:24:14 +0530

    datatype summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,46 @@
+// Basically data types are divided into two types
+
+// PRIMITIVE & NON-PRIMITIVE(REFERENCE TYPE)
+
+// basic difference is that :-
+
+// How the data is stored in a memory and how we can access it
+
+//  --------- PRIMITIVE DATA TYPES -----------
+//   {number, string, boolean, null, undefined, bigint, symbol}
+
+const num = 22
+const myString = "Jatin"
+const isLoggendIn = false
+let userState
+const bigNum = 2233n
+const id = Symbol('123')
+
+// ------- REFERENCE TYPE (NON-PRIMITIVE TYPE) ----------
+//  array, object, function
+
+const myArray = ["superman", "spiderman", "krish"]
+const myObject =  {
+    name: "jatin",
+    age: 21
+}
+const myFunction = function(){
+    console.log("hello world!")
+}
+
+
+// ------------------------------------------------------------
+
+// ~~~~~~~~~ Lets find out TYPEOF of every data type ~~~~~~~~~~
+
+console.log(typeof num);
+console.log(typeof myString);
+console.log(typeof isLoggendIn);
+console.log(typeof userState);
+console.log(typeof bigNum);
+console.log(typeof id);
+
+
+console.log(typeof myArray);
+console.log(typeof myObject);
+console.log(typeof myFunction); // output : function (but its called object-function)
